fcbb40f48497166baa9f09226285c33414946827
Victor Salazar <vsalazarco@utem.cl>
2025-09-03 19:52:03 -0400

    Clase 2 Completa

--- Fundamentos-DataScience-UTEM-main/Clase 2/Ejercicios Clase 2/Ejercicio 1.py
+++
@@ -1,9 +0,0 @@
-# Dado un array de temperaturas en grados Celsius grados_c = np.array([0, 15, 25, 30, 100]), conviértelas a grados Fahrenheit usando 
-# la fórmula $ F = C \times \frac{9}{5} + 32 $. Realiza la operación en una sola línea de código (vectorización).
-
-import numpy as np
-grados_c = np.array([0, 15, 25, 30, 100])
-grados_f = grados_c * (9/5) + 32
-
-print("Temperaturas en Celsius:", grados_c)
-print("Temperaturas en Fahrenheit:", grados_f)
--- Fundamentos-DataScience-UTEM-main/Clase 2/Librerias Clase 2/Libreria NumPy.py
+++
@@ -1,9 +0,0 @@
-# Convencion para importar NumPy
-import numpy as np
-
-# Crear un array de NumPy desde una lista
-lista_notas = [6.5, 7.0, 5.8, 4.9]
-array_notas = np.array(lista_notas)
-print(f"Las notas son: ",array_notas)
-notas_finales_np = array_notas + 0.5
-print(f"Las notas con 5 decimas mas son: ",notas_finales_np)
